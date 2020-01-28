9a15e5048ab26273c7e7c42008a60dfe678855f9
Kathleen Kamali <kathleenkamali@gmail.com>
2020-01-28 15:01:11 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function () {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+  const dogImages = document.getElementById('dog-image-container')
+  let breeds
+  let dogBreeds = document.getElementById('dog-breeds')
+  let selector = document.getElementById('breed-dropdown')
+
+  fetch(imgUrl)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (json) {
+      let images = json.message
+      for (let i = 0; i < images.length; i++) {
+        let image = document.createElement('img')
+        image.setAttribute('src', images[i])
+        dogImages.appendChild(image)
+      }
+    })
+
+  fetch(breedUrl)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (json) {
+      breeds = json.message
+      listBreeds()
+    })
+
+  selector.addEventListener('change', function (e) {
+    let filter = e.target.value
+    dogBreeds.innerHTML = ''
+    listBreeds(filter)
+  })
+
+  function listBreeds(filter = "none") {
+    for (var breed of Object.keys(breeds)) {
+      let li = document.createElement('li')
+      li.innerText = breed
+      if (filter == "none" || !breed.startsWith(filter)) {
+        dogBreeds.appendChild(li)
+      }
+      li.addEventListener('click', function () {
+        li.style.color = "hotpink"
+      })
+    }
+  }
+})
